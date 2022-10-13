9f77e12d66fb7f569d2ff033a3552016b0c771a1
athosportes <athosuti@gmail.com>
2022-10-13 18:28:06 -0300

    first commit

---
+++ src/test/java/com/school/workspace/WorkspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.workspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
