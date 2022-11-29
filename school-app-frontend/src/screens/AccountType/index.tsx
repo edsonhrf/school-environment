a02b948a31d8bc2ac32d74f05bf2e5a6afe50710
edsonhrf <edsonhrf@gmail.com>
2022-11-29 15:39:55 -0300

    account type animation

--- school-app-frontend/src/screens/AccountType/index.tsx
+++ school-app-frontend/src/screens/AccountType/index.tsx
@@ -1,80 +1,112 @@
 import * as React from 'react';
-import { TextInput, Button } from 'react-native-paper';
-import { View, Text, Image, } from "react-native";
+import { TextInput } from 'react-native-paper';
+import { Text, Image, TouchableOpacity, SafeAreaView} from "react-native";
 import { styles } from './styles';
+import { Ionicons } from '@expo/vector-icons'; 
+
 
 export function AccountType() {
     const [text, setText] = React.useState("");
+    const [teacher, setTeacher] = React.useState(false);
+    const [student, setStudent] = React.useState(false);
+
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
             
-            <View style={styles.containerLogo}>
+            <SafeAreaView style={styles.containerLogo}>
                 <Image
                     source={require('../../../assets/logo.png')}
                     style={{ width: '60%' }}
                     resizeMode='contain'
                 />
-            </View>
+            </SafeAreaView>
 
-            <View style={styles.containerSubtitle}>
+            <SafeAreaView style={styles.containerSubtitle}>
                 <Text
                     style={{ fontSize: 22, color: 'gray' }}
                 >
                     Choose your access type
                 </Text>
-            </View>
+            </SafeAreaView>
  
-            <View style={styles.containerAccountTypeButtons}>
+            <SafeAreaView style={styles.containerAccountTypeButtons}>
 
-                <Button style={styles.teacherButton}
-                    mode="elevated"
-                    onPress={() => console.log('Pressed')}
-                >  
-                    {/* <Image style={styles.imageTeacherButton}
-                        source={require('../../../assets/teacher.png')}
-                        resizeMode='contain'
-                    />     */}
-                </Button>
+                <TouchableOpacity 
+                    style={teacher ? styles.teacherButtonSelected : styles.teacherButton}
+                    onPress={() => {setTeacher(!teacher); setStudent(false)}}>
 
-                <Button style={styles.studentButton}
-                    mode="elevated"
-                    onPress={() => console.log('Pressed')}
-                >
-                    {/* <Image style={styles.imageStudentButton}
-                        source={require('../../../assets/person.png')}
-                        resizeMode='contain'
-                    /> */}
-                </Button>
-            </View>
+                        <Image
+                            blurRadius={teacher ? 0 : 2}
+                            style={styles.imageTeacherButton}
+                            source={require('../../../assets/teacher.png')}
+                            resizeMode='contain'
+                        />   
 
-            <View style={styles.containerForm}>
+                        <SafeAreaView style={styles.containerTextAndIconTeacherButton}>
+                            <Text 
+                                style={teacher ? styles.textTeacherButtonSelected : styles.textTeacherButton}>
+                                    Teacher 
+                            </Text>
+                            <Ionicons
+                                style={teacher ? styles.checkedIconTeacherButtonSelected : styles.checkedIconTeacherButton}
+                                name="checkmark-circle-sharp"
+                                size={24}
+                                color="green"
+                            />
+                        </SafeAreaView>
+                </TouchableOpacity> 
+
+                <TouchableOpacity
+                    style={student ? styles.studentButtonSelected : styles.studentButton}
+                    onPress={() => {setStudent(!student); setTeacher(false)}}>
+
+                        <Image
+                            blurRadius={student ? 0 : 2}
+                            style={styles.imageStudentButton}
+                            source={require('../../../assets/person.png')}
+                            resizeMode='contain'
+                        />   
+
+                        <SafeAreaView style={styles.containerTextAndIconStudentButton}>
+                            <Text
+                                style={student ? styles.textStudentButtonSelected : styles.textStudentButton}>
+                                    Student 
+                            </Text>   
+                            <Ionicons
+                                style={student ? styles.checkedIconStudentButtonSelected : styles.checkedIconStudentButton}
+                                name="checkmark-circle-sharp"
+                                size={24}
+                                color="green"
+                            /> 
+
+                        </SafeAreaView>
+                </TouchableOpacity> 
+            </SafeAreaView>
+
+            <SafeAreaView style={styles.containerForm}>
                 <TextInput
                     keyboardType='decimal-pad'
                     mode='outlined'
                     label='Enrollment number'
                     selectionColor='#3A96AD'
                     activeOutlineColor='#3A96AD'
+                    // backgroundColor='white'
                     // value={text}
                     onChangeText={text => setText(text)}
                     left={<TextInput.Icon icon="account" />}
                 />
-            </View>
+            </SafeAreaView>
 
-            <View style={styles.containerContinueButton}>
-                <Button style={styles.continueButton}
-                    mode="contained"
-                    onPress={() => console.log('Clique no botão capturado')}
-                    buttonColor='#3A96AD'
-                    icon={'page-next-outline'}
-                    labelStyle={{fontSize: 30}}
-                    contentStyle={{flexDirection: 'row-reverse'}}
-                >
+            <SafeAreaView style={styles.containerContinueButton}>
+                <TouchableOpacity
+                    style={styles.continueButton}
+                    onPress={() => console.log('Clique no botão para continuar')}>
                         <Text style={styles.buttonText}>
                             Continue   
                         </Text>
-                </Button>
-            </View>
+                </TouchableOpacity>    
+            </SafeAreaView>
 
-        </View>
+        </SafeAreaView>
     )
 }
--- school-app-frontend/src/screens/AccountType/styles.ts
+++ school-app-frontend/src/screens/AccountType/styles.ts
@@ -34,38 +34,125 @@ export const styles = StyleSheet.create({
     teacherButton: {
         width: '45%',
         height: '70%',
-        margin: 5
+        margin: 5,
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        alignContent: 'center',
+        alignItems: 'center',
+        elevation: 10,
+        // activeOpacity: 1000
+    },
+    teacherButtonSelected: {
+        width: '45%',
+        height: '70%',
+        margin: 5,
+        backgroundColor: '#B0DCE7',
+        borderRadius: 20,
+        alignContent: 'center',
+        alignItems: 'center',
+        elevation: 10,
+        activeOpacity: 1000
     },
     imageTeacherButton: {
-        
+        width: '60%',
+        height: '70%',
+        marginTop: 10,
+        marginBottom: 5
+    },
+    textTeacherButton: {
+        fontSize: 15,
+        color: '#D0D0D0',
+    },
+    textTeacherButtonSelected: {
+        fontSize: 16,
+        color: 'gray',
+        marginRight: 2
+    },
+    containerTextAndIconTeacherButton: {
+        flexDirection: 'row',
+    },
+    checkedIconTeacherButton: {
+        display: 'none'
+    },
+    checkedIconTeacherButtonSelected: {
+        display: 'flex'
     },
     studentButton: {
         width: '45%',
         height: '70%',
-        margin: 5
+        margin: 5,
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        alignContent: 'center',
+        alignItems: 'center',
+        elevation: 10
+    },
+    studentButtonSelected: {
+        width: '45%',
+        height: '70%',
+        margin: 5,
+        backgroundColor: '#B0DCE7',
+        borderRadius: 20,
+        alignContent: 'center',
+        alignItems: 'center',
+        elevation: 10
     },
     imageStudentButton: {
-        
+        width: '70%',
+        height: '70%',
+        marginTop: 10,
+        marginBottom: 5
+    },
+    textStudentButton: {
+        fontSize: 15,
+        color: '#D0D0D0',
+    },
+    textStudentButtonSelected: {
+        fontSize: 16,
+        color: 'gray',
+        marginRight: 2
+    },
+    checkedIconStudentButton: {
+        display: 'none'
+    },
+    checkedIconStudentButtonSelected: {
+        display: 'flex'
+    },
+    containerTextAndIconStudentButton: {
+        flexDirection: 'row',
     },
     containerForm: {
         flex: 1.0,
         // backgroundColor: 'orange',
         padding: 15,
         display: 'flex',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginRight: 10,
+        marginLeft: 10
     },
     containerContinueButton: {
         flex: 2,
         // backgroundColor: 'gray',
-        padding: 15,
+        // padding: 15,
         display: 'flex',
         justifyContent: 'center'
     },
     continueButton: {
-        borderRadius: 8,
-        padding: 10
+        alignItems:'center',
+        backgroundColor: '#3A96AD',
+        height: 70,
+        marginTop:20,
+        marginBottom:10,
+        padding: 15,
+        // borderRadius: 8,
+        justifyContent: 'center',
+        display: 'flex',
+        marginRight: 0,
+        marginLeft: 0,
+        flexDirection: 'row-reverse'
     },
     buttonText: {
-        fontSize: 20
+        fontSize: 20,
+        color: 'white'
     }
   });
